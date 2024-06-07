cef02232590d8607be8369ade63c3828c1ea534b
gowrirajl <gowriraj.logabiraman@gmail.com>
2024-06-07 11:24:42 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,4 @@
+function showCourseDetails(courseId) {
+    // Redirect the user to the course details page with courseId as a query parameter
+    window.location.href = "course-details.html?courseId=" + courseId;
+}
